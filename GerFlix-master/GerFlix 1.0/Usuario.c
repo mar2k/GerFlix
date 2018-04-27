abe7988ca5783d3e45be3a235a9850427cf1ffe5
Rocio <rosa.muruchilaura@gmail.com>
2018-04-27 12:24:39 -0300

    usuarios y series de netflix

---
+++ GerFlix-master/GerFlix 1.0/Usuario.c
@@ -0,0 +1,92 @@
+
+#include "Usuario.h"
+
+void inicializarUsuariosEstado(eUsuario usuarios[], int cant)
+{
+    int i;
+
+    for(i=0; i<cant; i++)
+    {
+        usuarios[i].estado = 0;
+    }
+}
+void inicializarUsuariosHardCode(eUsuario usuarios[])
+{
+
+
+    int id[15] = {1000,1001,1002,1003,1004, 1005,1006,1007,1008,1009, 1010,1011,1012,1013,1014};
+    char nombre[][50]= {"Juan","Maria","Pedro","Vanesa","Jose","Luisa","Laura","Marcelo","German","Victoria","Dafne","Antonela","Gisele","Dario","Pedro"};
+
+    int serie[15] = {100,100,101,101,102,100,100,103,101,102,103,101,100,100,101};
+
+    int i;
+
+    for(i=0; i<15; i++)
+    {
+        usuarios[i].idUsuario=id[i];
+        usuarios[i].idSerie=serie[i];
+        usuarios[i].estado = 1;
+        strcpy(usuarios[i].nombre, nombre[i]);
+
+    }
+
+
+}
+
+void mostrarListaUsuarios(eUsuario usuarios[], int t)
+{
+    int i;
+    for (i=0; i<t; i++)
+    {
+        if(usuarios[i].estado==1)
+        {
+            printf("%d  %s  %d\n",usuarios[i].idUsuario, usuarios[i].nombre,usuarios[i].idSerie);
+        }
+    }
+}
+
+void mostrarUsuarioConSuSerie(eUsuario usuarios[], int tamU, eSerie series[], int tamS)
+{
+    int i;
+    int j;
+    for(i=0; i<tamU; i++)
+    {
+        for(j=0; j<tamS;j++)
+        {
+            if(usuarios[i].idSerie==series[j].idSerie)
+            {
+                if(usuarios[i].estado==1)
+                {
+                    printf("%d  %s  %d %s  %s  %d\n",usuarios[i].idUsuario, usuarios[i].nombre,usuarios[i].idSerie, series[j].nombre, series[j].genero,series[j].cantidadTemporadas);
+                    break;
+                }
+
+            }
+        }
+    }
+}
+
+
+void mostrarSerieConSuUsuario(eSerie series[], int tamS, eUsuario usuarios[], int tamU)
+{
+    int i;
+    int j;
+    for(i=0; i<tamS; i++)
+    {
+        for(j=0; j<tamU;j++)
+        {
+            if(series[i].idSerie==usuarios[j].idSerie)
+            {
+                if(series[i].estado==1)
+                {
+                    printf("%d %s  %s  %d  %d  %s \n",usuarios[j].idSerie, series[i].nombre, series[i].genero,series[i].cantidadTemporadas,usuarios[j].idUsuario, usuarios[j].nombre);
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+}
+
---
+++ GerFlix-master/GerFlix 1.0/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Usuario.h"
+#define TAMSERIE 20
+#define TAMUSUARIO 100
+
+int main()
+{
+    eSerie listaDeSeries[TAMSERIE];
+    eUsuario listaDeUsuarios[TAMUSUARIO];
+
+    inicializarSeriesEstado(listaDeSeries, TAMSERIE);
+    inicializarSeriesHardCode(listaDeSeries);
+
+    inicializarUsuariosEstado(listaDeUsuarios,TAMUSUARIO);
+    inicializarUsuariosHardCode(listaDeUsuarios);
+
+//  1. Mostrar el listado de series
+
+        mostrarListaSeries(listaDeSeries,TAMSERIE);
+        printf("-----------------------\n");
+ //   2. Mostrar el listado de usuarios
+
+        mostrarListaUsuarios(listaDeUsuarios,TAMUSUARIO);
+        printf("-----------------------\n");
+
+ //   3. Mostrar el listado de Usuarios con el nombre de la serie que ve
+
+        mostrarUsuarioConSuSerie(listaDeUsuarios,TAMUSUARIO,listaDeSeries,TAMSERIE);
+        printf("-----------------------\n");
+
+ //   4. Mostrar por cada serie, el nombre de los usuarios que la ven.
+
+        mostrarSerieConSuUsuario(listaDeSeries,TAMSERIE,listaDeUsuarios,TAMUSUARIO);
+        printf("-----------------------\n");
+
+
+    return 0;
+}
